6c9cf8c7b40614cdcc82ef814071b5ea94c49ebc
Xavier Salazar <xavierssalazar92@gmail.com>
2022-04-05 14:16:21 -0500

    adding nextui

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
+import { createRoot } from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "./index.scss";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <NextUIProvider>
+    <App tab="home" />
+  </NextUIProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
