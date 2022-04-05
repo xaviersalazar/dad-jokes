1c21dae3febbb92bacdadc615e1f299bbd76926e
Xavier Salazar <xavierssalazar92@gmail.com>
2022-04-05 14:24:17 -0500

    adding dad joke button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Button, Container } from "@nextui-org/react";
+
 function App() {
-  return <h1>Dad Jokes</h1>;
+  return (
+    <Container fluid>
+      <Button color="gradient" css={{ margin: "2rem auto" }}>
+        Random dad joke
+      </Button>
+    </Container>
+  );
 }
 
 export default App;
